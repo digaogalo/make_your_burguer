5566bc1d9204f96adf1c55e51912cd153a287d01
digaogalo <digaolimac@gmail.com>
2023-02-14 14:45:33 -0300

    header and footer style css

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <footer id="footer">
+        <p>Make Your Burger &copy;  
+            <a id="link" href="https://www.linkedin.com/in/rodrigolc/" target="_blank"> 
+                Rodrigo Carvalho 2023   
+            </a>  
+        </p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        width: 100%;
+    }
+    #link {
+        color: #FCBA03;
+        text-decoration: none;
+    }
+
+    #link:hover {
+        color: aliceblue;
+    }
+</style>
