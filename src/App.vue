ab311e5c52a9a17fe946625050bdef9c3a249dff
digaogalo <digaolimac@gmail.com>
2023-02-15 15:21:18 -0300

    banner component create

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <Navbar :logo="logo_src" :alt="app_name"/>
-  <router-view/>
-  <Footer />
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+      <router-view/>
+    <Footer />
+  </div>
 </template>
-
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+
 export default {
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
+  },
   components: {
     Navbar,
     Footer
-  },
-  data() {
-    return {
-      logo_src: '/img/logo.png',
-      app_name: 'Make Your Burguer'
-    }
   }
 }
 </script>
 
 <style>
+
   * {
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: Helvetica;
     padding: 0;
     margin: 0;
     box-sizing: border-box;
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,37 +1,26 @@
 <template>
-    <footer id="footer">
-        <p>Make Your Burger &copy;  
-            <a id="link" href="https://www.linkedin.com/in/rodrigolc/" target="_blank"> 
-                Rodrigo Carvalho 2023   
-            </a>  
-        </p>
-    </footer>
-</template>
-
-<script>
-export default {
-    name: 'Footer'
-}
-</script>
-
-<style scoped>
+    <div id="footer">
+      <p>Make Your Burger &copy; 2023</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Footer"
+  }
+  </script>
+  
+  <style scoped>
     #footer {
-        height: 200px;
-        background-color: #222;
-        border-top: 4px solid #111;
-        color: #FCBA03;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        bottom: 0;
-        width: 100%;
+      height: 200px;
+      background-color:#222;
+      border-top: 4px solid #111;
+      color: #fcba03;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
     }
-    #link {
-        color: #FCBA03;
-        text-decoration: none;
-    }
-
-    #link:hover {
-        color: aliceblue;
-    }
-</style>
+  </style>
